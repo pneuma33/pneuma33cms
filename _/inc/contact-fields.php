5c63e173b8d13ca56e993016568be3ec5c0c39d9
Ray Grau <rgrau@pneuma33.com>
2016-08-04 10:17:14 -0500

    updated contact fields

--- _/inc/contact-fields.php
+++ _/inc/contact-fields.php
@@ -10,7 +10,7 @@
 
 function cabbagecms_add_linkedin( $contactmethods ) {
   if ( !isset( $contactmethods['linkedinurl'] ) )
-	$contactmethods['linkedinurl'] = 'CabbageCMS LinkedIn URL';
+	$contactmethods['linkedinurl'] = 'LinkedIn URL';
  
   return $contactmethods;
 }
@@ -18,7 +18,7 @@ add_filter( 'user_contactmethods', 'cabbagecms_add_linkedin', 10, 1 );
 
 function cabbagecms_add_linkedinid( $contactmethods ) {
   if ( !isset( $contactmethods['linkedinid'] ) )
-	$contactmethods['linkedinid'] = 'CabbageCMS LinkedIn ID';
+	$contactmethods['linkedinid'] = 'LinkedIn ID';
  
   return $contactmethods;
 }
@@ -26,7 +26,7 @@ add_filter( 'user_contactmethods', 'cabbagecms_add_linkedinid', 10, 1 );
 
 function cabbagecms_add_twitterurl( $contactmethods ) {
   if ( !isset( $contactmethods['twitterurl'] ) )
-	$contactmethods['twitterurl'] = 'CabbageCMS Twitter URL';
+	$contactmethods['twitterurl'] = 'Twitter URL';
  
   return $contactmethods;
 }
@@ -34,47 +34,15 @@ add_filter( 'user_contactmethods', 'cabbagecms_add_twitterurl', 10, 1 );
 
 function cabbagecms_add_twitterid( $contactmethods ) {
   if ( !isset( $contactmethods['twitterid'] ) )
-	$contactmethods['twitterid'] = 'CabbageCMS Twitter ID';
+	$contactmethods['twitterid'] = 'Twitter ID';
  
   return $contactmethods;
 }
 add_filter( 'user_contactmethods', 'cabbagecms_add_twitterid', 10, 1 );
 
-function cabbagecms_add_title( $contactmethods ) {
-  if ( !isset( $contactmethods['title'] ) )
-	$contactmethods['title'] = 'CabbageCMS Title';
- 
-  return $contactmethods;
-}
-add_filter( 'user_contactmethods', 'cabbagecms_add_title', 10, 1 );
-
-function cabbagecms__add_role( $contactmethods ) {
-  if ( !isset( $contactmethods['cabbagecms_role'] ) )
-	$contactmethods['cabbagecms_role'] = 'CabbageCMS Role';
- 
-  return $contactmethods;
-}
-add_filter( 'user_contactmethods', 'cabbagecms__add_role', 10, 1 );
-
-function cabbagecms_add_photo( $contactmethods ) {
-  if ( !isset( $contactmethods['photo'] ) )
-	$contactmethods['photo'] = 'CabbageCMS Photo (id)';
- 
-  return $contactmethods;
-}
-add_filter( 'user_contactmethods', 'cabbagecms_add_photo', 10, 1 );
-
-function cabbagecms_add_caption( $contactmethods ) {
-  if ( !isset( $contactmethods['cabbagecms_caption'] ) )
-	$contactmethods['cabbagecms_caption'] = 'CabbageCMS Caption (id)';
- 
-  return $contactmethods;
-}
-add_filter( 'user_contactmethods', 'cabbagecms_add_caption', 10, 1 );
-
 function cabbagecms_add_facebook( $contactmethods ) {
   if ( !isset( $contactmethods['facebookurl'] ) )
-	$contactmethods['facebookurl'] = 'CabbageCMS Facebook URL';
+  $contactmethods['facebookurl'] = 'Facebook URL';
  
   return $contactmethods;
 }
@@ -82,8 +50,40 @@ add_filter( 'user_contactmethods', 'cabbagecms_add_facebook', 10, 1 );
 
 function cabbagecms_add_googleplus( $contactmethods ) {
   if ( !isset( $contactmethods['googleplusurl'] ) )
-	$contactmethods['googleplusurl'] = 'CabbageCMS Google+ URL';
+  $contactmethods['googleplusurl'] = 'Google+ URL';
  
   return $contactmethods;
 }
 add_filter( 'user_contactmethods', 'cabbagecms_add_googleplus', 10, 1 );
+
+function cabbagecms_add_title( $contactmethods ) {
+  if ( !isset( $contactmethods['title'] ) )
+	$contactmethods['title'] = 'Job Title';
+ 
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'cabbagecms_add_title', 10, 1 );
+
+function cabbagecms__add_role( $contactmethods ) {
+  if ( !isset( $contactmethods['cabbagecms_role'] ) )
+	$contactmethods['cabbagecms_role'] = 'Job Role';
+ 
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'cabbagecms__add_role', 10, 1 );
+
+function cabbagecms_add_photo( $contactmethods ) {
+  if ( !isset( $contactmethods['photo'] ) )
+	$contactmethods['photo'] = 'Profile Photo (id #)';
+ 
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'cabbagecms_add_photo', 10, 1 );
+
+function cabbagecms_add_caption( $contactmethods ) {
+  if ( !isset( $contactmethods['cabbagecms_caption'] ) )
+	$contactmethods['cabbagecms_caption'] = 'Caption (id #)';
+ 
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'cabbagecms_add_caption', 10, 1 );
